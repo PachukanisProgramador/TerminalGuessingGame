93bda255dc54a2d71e7ce8727cf6390db66aec6a
PachukanisProgramador <thiago.leitao@direitosbc.br>
2022-06-08 16:22:03 -0300

    Atualizando jogo

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,18 +1,48 @@
-print('=================================\nBem-vindo ao jogo de adivinhação!\n=================================')
+import this
+
+print('=======================\nBem-vindo ao jogo de adivinhação!\n=======================')
 numero_secreto = 42
 
 looping = True
+total_tentativas = 15
 
-while looping:
-    chute = int(input('Digite o número que você acredita ser idêntico ao número secreto: '))
+while total_tentativas:
+    chute = int(input('Digite o número que você acredita ser idêntico ao número secreto entre 1 e 100: '))
 
-    if numero_secreto == chute:
-        print('Número digitado : {}\nNúmero secreto: {}\n Você acertou! Parabéns!'.format(chute, numero_secreto))
+    if chute < 1 or chute > 100:
+        print('Valor inválido.', end=' ')
+    else:
+        acertou = numero_secreto == chute
 
-        resposta = int(input('Fim do jogo!\n\nJogar novamente?\n1. Sim\n2. Não\n'))
-        if resposta != 1:
-            looping = False
-    elif numero_secreto > chute:
-        print('Que pena, você errou!\n {} é menor que o número secreto'.format(chute))
-    elif numero_secreto < chute:
-        print('Que pena, você errou!\n {} é maior que o número secreto'.format(chute))
+        if acertou:
+            print(f'Correto!\n{numero_secreto} é o número secreto! Parabéns!')
+
+            this.resposta = 0
+            while this.resposta != 1 or this.resposta != 2:
+                this.resposta = int(input('Fim do jogo!\n\nJogar novamente?\n1. Sim\n2. Não\n'))
+
+                if this.resposta == 2:
+                    print('Obrigado por jogar!')
+                    break
+                elif this.resposta == 1:
+                    total_tentativas = 15
+                    break
+                else:
+                    print('Comando inválido. Digite novamente')
+            if this.resposta == 2:
+                break
+        elif numero_secreto > chute:
+            total_tentativas -= 1
+            print(f'Que pena, você errou!\nO número {chute} é menor que o número secreto\n\nTentativas restantes: {total_tentativas}')
+
+        else:
+            print(f'Que pena, você errou!\nO número {chute} é maior que o número secreto\n\nTentativas restantes: {total_tentativas}')
+            total_tentativas -= 1
+
+    if total_tentativas == 0:
+        resposta = int(input('Que pena, todas as suas tentativas esgotaram.\nJogar novamente? 1. Sim\n2. Não'))
+        if resposta == 1:
+            total_tentativas = 15
+        else:
+            print('Obrigado por jogar!')
+            break
